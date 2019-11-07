b412c5e38f7dd5bd23061b65b08913fdaf2e1f2a
Dave Baskind <dave@picketstudio.com>
2019-11-08 08:58:49 +1100

    Accept and set an optional database file argument from process.env.TEST_DATABASE

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
+const dataSource = process.env.TEST_DATABASE || './database.sqlite';
+console.log(dataSource);
 module.exports = app;
