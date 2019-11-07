d2cec9d2ff62a392a32a72b43efe7b4644ca6a45
Dave Baskind <dave@picketstudio.com>
2019-11-08 09:00:31 +1100

    Use the root-level database.sqlite as your API's database

--- server.js
+++ server.js
@@ -2,5 +2,4 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 const dataSource = process.env.TEST_DATABASE || './database.sqlite';
-console.log(dataSource);
 module.exports = app;
